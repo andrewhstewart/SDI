9a5996d8a5dab66f7580880902f081c6bec1d181
Andrew Stewart <andrew.henry.stewart@emory.edu>
2018-06-29 20:36:44 -0400

    Fixed IRAF/PYRAF Dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README", "r") as fh:
     long_description = fh.read()
 
 setup(name='sdi_pipeline',
-      version='2.0',
+      version='2.1',
       description='Optical SETI Image Subtraction Pipeline',
       long_description=long_description,
       author='Andrew Stewart',
